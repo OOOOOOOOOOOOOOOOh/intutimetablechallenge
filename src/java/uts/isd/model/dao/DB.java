528649ca7a3411150135865dd8fbdb6d11698922
samuel.lim2 <samuel.lim2@192.168.20.13>
2022-06-19 22:53:51 +1000

    AWS RDS

--- src/java/uts/isd/model/dao/DB.java
+++ src/java/uts/isd/model/dao/DB.java
@@ -9,7 +9,8 @@ import java.sql.Connection;
 public abstract class DB {
 //    protected String URL = "jdbc:derby://localhost:1527/";
     protected String URL = "jdbc:mysql://aa1boye1ic9liso.clfkr0w3yl3q.ap-southeast-2.rds.amazonaws.com:3306";
-    protected String db = "iotdb";   
+//    protected String db = "iotdb";   
+    protected String db = "aa1boye1ic9liso";   
     protected String dbuser = "isduser";
     protected String dbpass = "admin123";
 //    protected String driver = "org.apache.derby.jdbc.ClientDriver";
